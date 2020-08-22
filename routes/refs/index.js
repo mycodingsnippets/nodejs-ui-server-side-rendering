70d5a16558db3a0a4cb4e9438ac74fecc6ba9fa3
Aditya <mail.aditya.bansal@gmail.com>
2020-08-22 16:00:54 +0530

    Adding columns

--- routes/refs/index.js
+++ routes/refs/index.js
@@ -36,6 +36,20 @@ router.get('/navbar', (req, res) => {
             '/css/refs/navbar.css'
         ]
     });
-})
+});
+
+router.get('/columns', (req, res) => {
+    res.render('pages/refs/columns', {
+        title: 'Columns',
+        scripts: [
+            ...commonJs,
+            '/js/refs/columns.js'
+        ],
+        styles: [
+            ...commonCss,
+            '/css/refs/columns.css'
+        ]
+    });
+});
 
 module.exports = router;
