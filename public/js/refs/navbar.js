e1b7d96b2a4210407f3abb3fcaa78c73cbf50d0d
Aditya <mail.aditya.bansal@gmail.com>
2020-08-22 13:46:16 +0530

    Completed navbar using CSS grid

---
+++ public/js/refs/navbar.js
@@ -0,0 +1,3 @@
+document.querySelector('#menu-btn').addEventListener('click', function () {
+    document.getElementById('mobile-menu').style.display = document.getElementById('mobile-menu').style.display === 'none' ? 'block': 'none';
+});
