46558edf7e75fe63af73cda75504fd7a106b4714
Aditya <mail.aditya.bansal@gmail.com>
2020-09-02 00:39:44 +0530

    Adding support for post requests

--- services/dummyService.js
+++ services/dummyService.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/dummy', async (req, res) => {
-    let result = await axios({url: 'https://reqres.in/api/users/2',method: 'GET',headers: {},params: {}});
+    let result = await axios({url: 'https://reqres.in/api/users/2',method: 'GET',headers: {},data: {}});
     res.send({
         data: result.data
     })
