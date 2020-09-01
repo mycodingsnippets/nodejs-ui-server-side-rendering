ee5efaa70357cced6865d579e03780eefe1f23ce
Aditya <mail.aditya.bansal@gmail.com>
2020-09-02 00:07:41 +0530

    Prepared Structure for dummy service

--- services/dummyService.js
+++ services/dummyService.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/dummy', (req, res) => {
-    res.json({
-        name: 'Aditya'
-    });
+router.get('/dummy', async (req, res) => {
+    let result = await axios({url: 'https://reqres.in/api/users/2',method: 'GET',headers: {},params: {}});
+    res.send({
+        data: result.data
+    })
 });
 
 module.exports = router;
