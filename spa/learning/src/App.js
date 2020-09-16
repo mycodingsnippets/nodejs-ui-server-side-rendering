77c1cfcab602e79373d66feb5810c41593e9581f
Aditya <mail.aditya.bansal@gmail.com>
2020-09-16 23:55:16 +0530

    Playing with react

--- spa/learning/src/App.js
+++ spa/learning/src/App.js
@@ -1,33 +1,10 @@
 import React from 'react';
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Message from "./components/Message";
-import FunctionClick from "./components/FunctionClick";
-import EventBind from "./components/EventBind";
-import ParentComponent from "./components/ParentComponent";
-import UserGreeting from "./components/UserGreeting";
-import NameList from "./components/NameList";
-import Stylesheet from "./components/Stylesheet";
-import Form from "./components/Form";
+import RegistrationForm from "./components/uicomps/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
-        <Greet name="Aditya">
-            <p>You inspire us a developer</p>
-        </Greet>
-        <Welcome name="Best"/>
-        <Message name="Aditya"/>
-        <hr/>
-        <FunctionClick/>
-        <EventBind/>
-        <hr/>
-        <ParentComponent/>
-        <UserGreeting/>
-        <hr/>
-        <NameList/>
-        <Stylesheet/>
-        <Form/>
+        <RegistrationForm />
     </div>
   );
 }
